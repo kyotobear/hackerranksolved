04ca5e15f92c7d8ebd25562eaec9c0b094bb6dc7
kyotobear <kiytobear@gmail.com>
2022-05-26 23:23:36 -0500

    update code

---
+++ solveproblems.py
@@ -0,0 +1,38 @@
+""""
+Given an integer, 
+ perform the following conditional actions:
+If  is odd, print Weird
+If  is even and in the inclusive range of  to,
+ print Not Weird
+If  is even and in the inclusive range of to , 
+print Weird
+If  is even and greater than , print Not Weird
+"""
+# if __name__ == '__main__':
+#     n = int(input().strip())
+    
+#     if n % 2 != 0:
+#         print("Weird")
+#     elif n % 2 == 0 and n in range(2,6):
+#         print("Not Weird")
+#     elif n % 2 == 0 and n in range(6, 21):
+#         print("Weird")
+#     elif n % 2 == 0 and n > 20:
+#         print("Not Weird")
+
+"""The provided code stub reads two integers,  and , from STDIN.
+Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+No rounding or formatting is necessary.
+
+Example:
+The result of the integer division .
+The result of the float division is ."""
+
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     c = a // b
+#     d = a / b
+#     if (a,b):
+#         print(c)
+#         print(d)
